88fa365a0ed52fcaa9fe813b375acea1f6c97aab
Rivendor <flimarques26@gmail.com>
2019-09-09 13:18:21 -0300

    Actually final commit

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,7 +1,3 @@
-// Altera tag h1 com classe .titulo
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
-
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++)
